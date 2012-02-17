f6f77f1912163183a0a26085f18d94b667bcec77
Tassilo Horn <horn@uni-koblenz.de>
2012-02-17 12:35:13 +0100

    Added jamopp stuff

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject kemerer-chidamber "1.0.0"
   :description "The Kemerer & Chidamber metrics for GraBaJa syntax graphs."
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0-beta1"]
                  [funnyqt "[0.0.2,)"]]
   :warn-on-reflection true
   :jvm-opts ["-Xmx1G"])
--- test/kemerer_chidamber/jamopp/test/core.clj
+++ test/kemerer_chidamber/jamopp/test/core.clj
@@ -1,4 +1,28 @@
 (ns kemerer-chidamber.jamopp.test.core
   (:use [kemerer-chidamber.jamopp.core])
-  (:use [funnyqt.tg.core])
-  (:use [clojure.test]))
+  (:use [funnyqt.emf.core])
+  (:use [clojure.test])
+  (:require [clojure.java.io :as io]))
+
+(defn register-jamopp-metamodel
+  [ecore-file]
+  (load-metamodel ecore-file))
+
+(register-jamopp-metamodel "java.ecore")
+
+(defn gunzip
+  [fi fo]
+  (with-open [i (io/reader
+                 (java.util.zip.GZIPInputStream.
+                  (io/input-stream fi)))
+              o (java.io.PrintWriter. (io/writer fo))]
+    (doseq [l (line-seq i)]
+      (.println o l))))
+
+(defn load-jgralab-model
+  []
+  (when-not (.exists (io/file "jgralab.xmi"))
+    (gunzip "jgralab.xmi.gz" "jgralab.xmi"))
+  (load-model "jgralab.xmi"))
+
+(def jm (memoize load-jgralab-model))
