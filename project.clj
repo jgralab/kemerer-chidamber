930bd0788c750af141f33a769323d78572993235
Tassilo Horn <horn@uni-koblenz.de>
2012-02-22 12:16:35 +0100

    require newer funnyqt release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject kemerer-chidamber "1.0.0"
   :description "The Kemerer & Chidamber metrics for GraBaJa & JaMoPP syntax graphs."
   :dependencies [[org.clojure/clojure "1.4.0-beta1"]
-                 [funnyqt "[0.0.4,)"]]
+                 [funnyqt "[0.0.5,)"]]
   :warn-on-reflection true
   :jvm-opts ["-Xmx1G"])
