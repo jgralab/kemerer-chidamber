fb00f6ac41e5cdb6a5d601b38617bd603480d6ff
Tassilo Horn <horn@uni-koblenz.de>
2012-08-17 12:23:38 +0200

    add license and stuff

--- project.clj
+++ project.clj
@@ -2,6 +2,11 @@
   :description "The Kemerer & Chidamber metrics for GraBaJa & JaMoPP syntax graphs."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [funnyqt "0.2.5"]]
+  :license {:name "GNU General Public License, Version 3"
+            :url "http://www.gnu.org/copyleft/gpl.html"
+            :distribution :repo}
+  :url "https://github.com/jgralab/kemerer-chidamber"
   :warn-on-reflection true
+  :jar-exclusions [#"(?:^|/).(svn|hg|git|tg|tg\.gz|xmi|xmi\.gz)/"]
   :main kemerer-chidamber.main
   :jvm-opts ["-Xmx2G"])
