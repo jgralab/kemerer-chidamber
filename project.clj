c97e9ffe669af80c31667c68189b6d0eb50192ef
Tassilo Horn <horn@uni-koblenz.de>
2012-08-17 11:25:33 +0200

    bump to 1.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kemerer-chidamber "1.1.0"
+(defproject kemerer-chidamber "1.1.1"
   :description "The Kemerer & Chidamber metrics for GraBaJa & JaMoPP syntax graphs."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [funnyqt "0.2.5"]]
